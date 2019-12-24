03aed6339891e049f0a57420609c833d2a056d2f
Hikaru <nikotyan77@icloud.com>
2019-12-24 17:24:22 +0900

    [finish]3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+      @book = Book.new(book_params)
+      if @book.save
+        flash[:success] = 'Book was successfully created.'
+        redirect_to book_path(@book)
+      else
+        @books = Book.all
+        render :index
+      end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:success] = 'Book was successfully updated.'
+    redirect_to book_path(book)
+  end
+
+  def destroy	
+        book = Book.find(params[:id])
+        book.destroy
+        redirect_to books_path
+    end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
+
